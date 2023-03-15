899760f003cd2694a18981e66c35254d7775e606
József Bagota <66431498+bagotaj@users.noreply.github.com>
2023-03-15 21:09:50 +0100

    design upgrade with css and new components

--- src/Templates/Welcome/articles.js
+++ src/Templates/Welcome/articles.js
@@ -1,67 +1,60 @@
 export default {
-    "type": "description-section-container",
-    "data": {
-        "description_sections": [
-            {
-                "type": "dynamic-web-description-section",
-                "data": {
-                    "text_content_section": {
-                        "type": "small-black50-text-content",
-                        "data": {
-                            "title": "--welcome.article1.title",
-                            "content": "--welcome.article1.content",
-                            "added_classes": {
-                                ".content": [
-                                    "mb-0", 
-                                    "text-start"
-                                ]
-                            }
-                        }
-                    },
-                    "image_url": "images/Articles/blonde-woman-floral-dress-cafe.jpg",
-                    "image_alt": "..."
-                }
+  type: 'description-section-container',
+  data: {
+    description_sections: [
+      {
+        type: 'dynamic-web-description-section',
+        data: {
+          text_content_section: {
+            type: 'dynamic-web-articles-small-text-content',
+            data: {
+              title: '--welcome.article1.title',
+              content: '--welcome.article1.content',
+              added_classes: {
+                '.content': ['mb-0'],
+              },
             },
-            {
-                "type": "dynamic-web-description-section",
-                "data": {
-                    "text_content_section": {
-                        "type": "small-black50-text-content",
-                        "data": {
-                            "title": "--welcome.article2.title",
-                            "content": "--welcome.article2.content",
-                            "added_classes": {
-                                ".content": [
-                                    "mb-0", 
-                                    "text-end"
-                                ]
-                            }
-                        }
-                    },
-                    "image_url": "images/Articles/e-commerce-shop-online-homepage-sale-concept.jpg",
-                    "image_alt": "..."
-                }
+          },
+          image_url: 'images/Articles/blonde-woman-floral-dress-cafe.jpg',
+          image_alt: '...',
+        },
+      },
+      {
+        type: 'dynamic-web-description-section',
+        data: {
+          text_content_section: {
+            type: 'dynamic-web-articles-small-text-content',
+            data: {
+              title: '--welcome.article2.title',
+              content: '--welcome.article2.content',
+              added_classes: {
+                '.content': ['mb-0'],
+              },
             },
-            {
-                "type": "dynamic-web-description-section",
-                "data": {
-                    "text_content_section": {
-                        "type": "small-black50-text-content",
-                        "data": {
-                            "title": "--welcome.article3.title",
-                            "content": "--welcome.article3.content",
-                            "added_classes": {
-                                ".content": [
-                                    "mb-0", 
-                                    "text-start"
-                                ]
-                            }
-                        }
-                    },
-                    "image_url": "images/Articles/hands-holding-credit-card-typing-laptop.jpg",
-                    "image_alt": "..."
-                }
-            }
-        ]
-    }
-}
+          },
+          image_url:
+            'images/Articles/e-commerce-shop-online-homepage-sale-concept.jpg',
+          image_alt: '...',
+        },
+      },
+      {
+        type: 'dynamic-web-description-section',
+        data: {
+          text_content_section: {
+            type: 'dynamic-web-articles-small-text-content',
+            data: {
+              title: '--welcome.article3.title',
+              content: '--welcome.article3.content',
+              added_classes: {
+                '.content': ['mb-0'],
+              },
+            },
+          },
+          image_url:
+            'images/Articles/hands-holding-credit-card-typing-laptop.jpg',
+          image_alt: '...',
+        },
+      },
+    ],
+  },
+};
