34ad70c10899b765daa493d7980b7e0cea136086
József Bagota <66431498+bagotaj@users.noreply.github.com>
2023-03-17 16:22:45 +0100

    making something

---
+++ src/ContactForm/FormItems/Inputs/CheckBoxWithTextContent/Component.vue
@@ -0,0 +1,26 @@
+<script>
+    import CheckBoxInput from './../../../../../../helper-vue-components/src/Form/BootstrapFormItems/FormItems/Inputs/CheckBox/Component.vue'
+    import SmallTextContent from './../../../../../../text-contents-vue-components/src/SmallTextContent/Component.vue'
+
+    export default {
+        mixins: [
+            CheckBoxInput,
+            SmallTextContent
+        ],
+        props: {
+            added_classes: {
+                type: Object,
+                default () {
+                    return{
+                        '.title': 'text-uppercase',
+                        '.content': 'text-muted'
+                    }
+                }
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    @import "Style.scss";
+</style>
