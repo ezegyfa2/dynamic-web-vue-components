e5b2dcedced852d1967d04a0e2d673adb86de66e
József Bagota <jozsef.bagota@gmail.com>
2023-10-12 17:26:13 +0200

    adding vue-image component

--- src/Welcome/LatestWorks/index.js
+++ src/Welcome/LatestWorks/index.js
@@ -3,7 +3,8 @@ export default {
         window.templates.dynamic_web_latest_works = require('./Template').default
         require('dynamic-web-vue-components/GlobalComponents/MediumTextContent').default.install(Vue)
         require('dynamic-web-vue-components/GlobalComponents/MediumTextContentWithButton').default.install(Vue)
-        
+        require('helper-vue-components/image').default.install(Vue)
+
         Vue.component('dynamic-web-latest-works', () => import('./Component.vue'))
     }
 }
